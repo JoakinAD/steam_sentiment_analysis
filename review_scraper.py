05769eefdcf49786cd0a9e8d94f1f635a7cee9c7
JoakinAD <joakinabadiaz@gmail.com>
2025-07-16 02:46:34 +0200

    Isolated 8 steam reviews

--- review_scraper.py
+++ review_scraper.py
@@ -6,7 +6,11 @@ def scrape():
     url = 'https://steamcommunity.com/app/3527290/positivereviews/?browsefilter=toprated&snr=1_5_100010_'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(soup)
+    reviews = soup.find_all("div", {"class":"apphub_CardContentMain"})
+    for review in reviews:
+        for string in review.stripped_strings:
+            print(string)
+        print("\n\n")
 
 if __name__ == '__main__':
     scrape()
